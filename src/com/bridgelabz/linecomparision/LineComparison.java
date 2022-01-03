1e26baacbd652e974521814ed3d2c6d4e6c88681
Textspot <sachinkatarki1995@gmail.com>
2022-01-03 22:07:48 +0530

    Added:Welcome to the Line Comparision Programme

---
+++ src/com/bridgelabz/linecomparision/LineComparison.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.linecomparision;
+
+import java.util.Scanner;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line comparison program");
+		
+	}
+}
