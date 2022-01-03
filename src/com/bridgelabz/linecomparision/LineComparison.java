a8d3b6eb3e398438cb792ed8ddfe2acb43763908
Textspot <sachinkatarki1995@gmail.com>
2022-01-03 22:32:15 +0530

    Added:Code to Check the Equality of Two lines

--- src/com/bridgelabz/linecomparision/LineComparison.java
+++ src/com/bridgelabz/linecomparision/LineComparison.java
@@ -3,25 +3,40 @@ package com.bridgelabz.linecomparision;
 import java.util.Scanner;
 
 public class LineComparison {
+	public static int x1, x2, x3, x4, y1, y2, y3, y4;
+	public static double distance1, distance2;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line comparison program");
 		lengthofLine();
-	}
-		public static void lengthofLine() {
-	        Scanner sc = new Scanner(System.in);
-	        System.out.print("enter the value of x1 = ");
-	        int x1 = sc.nextInt();
-	        System.out.print("enter the value of x2 = ");
-	        int x2 = sc.nextInt();
-	        System.out.print("enter the value of y1 = ");
-	        int y1 = sc.nextInt();
-	        System.out.print("enter the value of y2 = ");
-	        int y2 = sc.nextInt();
-
-	        double distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-	        System.out.println("Distance = " + distance);
-	    }
-		
+		distance();
 	}
 
+	public static void lengthofLine() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter the value of x1 = ");
+		x1 = sc.nextInt();
+		System.out.print("enter the value of x2 = ");
+		x2 = sc.nextInt();
+		System.out.print("enter the value of x3 = ");
+		x3 = sc.nextInt();
+		System.out.print("enter the value of x4 = ");
+		x4 = sc.nextInt();
+		System.out.print("enter the value of y1 = ");
+		y1 = sc.nextInt();
+		System.out.print("enter the value of y2 = ");
+		y2 = sc.nextInt();
+		System.out.print("enter the value of y3 = ");
+		y3 = sc.nextInt();
+		System.out.print("enter the value of y4 = ");
+		y4 = sc.nextInt();
+	}
+
+	public static void distance() {
+		distance1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		System.out.println("Distance 1 = " + distance1);
+		distance2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+		System.out.println("Distance 2 = " + distance2);
+
+	}
+}
